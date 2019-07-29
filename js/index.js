f62090ca37888cf2087971a79a006331807e5f9c
Axel <axel.gordet.pop@gmail.com>
2019-07-29 19:27:54 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,137 @@
+function marron()
+{
+    var audio = new Audio('audio/un-marron.mp3');
+    audio.play();
+}
+
+function gueule()
+{
+    var audio = new Audio('audio/bousille-sa-gueule.mp3');
+    audio.play();
+}
+
+function con()
+{
+    var audio = new Audio('audio/ces_con.mp3');
+    audio.play();
+}
+
+function corbeau()
+{
+    var audio = new Audio('audio/corbeau-decede.mp3');
+    audio.play();
+}
+
+function beche()
+{
+    var audio = new Audio('audio/coup_de_beche.mp3');
+    audio.play();
+}
+
+function merlin()
+{
+    var audio = new Audio('audio/cv-merlin.mp3');
+    audio.play();
+}
+
+function mouille()
+{
+    var audio = new Audio('audio/dans-sa-mouille.mp3');
+    audio.play();
+}
+
+function connard()
+{
+    var audio = new Audio('audio/espece-de-connard.mp3');
+    audio.play();
+}
+
+function faux()
+{
+    var audio = new Audio('audio/faux.mp3');
+    audio.play();
+}
+
+function uni()
+{
+    var audio = new Audio('audio/fils-dunijambiste-1.mp3');
+    audio.play();
+}
+
+function noix()
+{
+    var audio = new Audio('audio/les-noix.mp3');
+    audio.play();
+}
+
+function gros()
+{
+    var audio = new Audio('audio/on_en_a_gros-1-1-1.mp3');
+    audio.play();
+}
+
+function rocher()
+{
+    var audio = new Audio('audio/rocher.mp3');
+    audio.play();
+}
+
+function caillou()
+{
+    var audio = new Audio('audio/tas-de-callious.mp3');
+    audio.play();
+}
+
+function conx2()
+{
+    var audio = new Audio('audio/vous_etes_completement_con.mp3');
+    audio.play();
+}
+
+
+
+
+let p= document.getElementById("btn_marron");
+p.addEventListener("click", marron);
+
+let p2= document.getElementById("btn_gueule");
+p2.addEventListener("click", gueule);
+
+let p3= document.getElementById("btn_con");
+p3.addEventListener("click", con);
+
+let p4= document.getElementById("btn_corbeau");
+p4.addEventListener("click", corbeau);
+
+let p5= document.getElementById("btn_beche");
+p5.addEventListener("click", beche);
+
+let p6= document.getElementById("btn_merlin");
+p6.addEventListener("click", merlin);
+
+let p7= document.getElementById("btn_mouille");
+p7.addEventListener("click", mouille);
+
+let p8= document.getElementById("btn_connard");
+p8.addEventListener("click", connard);
+
+let p9= document.getElementById("btn_faux");
+p9.addEventListener("click", faux);
+
+let p10= document.getElementById("btn_uni");
+p10.addEventListener("click", uni);
+
+let p11= document.getElementById("btn_noix");
+p11.addEventListener("click", noix);
+
+let p12= document.getElementById("btn_gros");
+p12.addEventListener("click", gros);
+
+let p13= document.getElementById("btn_rocher");
+p13.addEventListener("click", rocher);
+
+let p14= document.getElementById("btn_caillou");
+p14.addEventListener("click", caillou);
+
+let p15= document.getElementById("btn_conx2");
+p15.addEventListener("click", conx2);
